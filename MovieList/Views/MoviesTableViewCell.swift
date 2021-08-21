42bacfa9b3f74eb87726f5513016e88343a14075
brainox <macmartins081@gmail.com>
2021-08-21 01:12:30 +0100

    added activityIndicator

--- MovieList/Views/MoviesTableViewCell.swift
+++ MovieList/Views/MoviesTableViewCell.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class MoviesTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }

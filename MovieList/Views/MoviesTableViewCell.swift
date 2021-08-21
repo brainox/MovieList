917fdf7d5be720992c0cf04ba63361b4e4b830ec
brainox <macmartins081@gmail.com>
2021-08-21 01:02:30 +0100

    created UI on storyboard and constrained it

---
+++ MovieList/Views/MoviesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesTableViewCell.swift
+//  MovieList
+//
+//  Created by Decagon on 21/08/2021.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
